63d1d9ceff7f2edb67c1dfa616c021c1a21c1c24
brenda_braganca <bragancadeassis@gmail.com>
2025-01-15 11:19:47 -0300

    feat: adaptando coluna img_url no banco de dados

--- src/main/resources/db/migration/V1__create-event-table.sql
+++ src/main/resources/db/migration/V1__create-event-table.sql
@@ -4,7 +4,7 @@ CREATE TABLE event (
     id UUID DEFAULT gEN_random_uuid() PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     description VARCHAR(250) NOT NULL,
-    img_url VARCHAR(100) NOT NULL,
+    img_url VARCHAR(400) NOT NULL,
     event_url VARCHAR(100) NOT NULL,
     date TIMESTAMP NOT NULL,
     remote BOOLEAN NOT NULL
